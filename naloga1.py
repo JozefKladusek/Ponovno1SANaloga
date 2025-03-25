0413ccb7f7626aa5cae3dc20a1d77ac686c7eb7b
Jozef <jozef.kladusek@student.um.si>
2025-03-25 13:56:03 +0100

    priprava main in funkcij

---
+++ naloga1.py
@@ -0,0 +1,55 @@
+import cv2 as cv
+import numpy as np
+
+levo_zgorajx = 0
+levo_zgorajy = 0
+desno_spodajx = 0
+desno_spodajy = 0
+selection_complete = False
+
+def mouse_callback(event, x, y, flags, slika):
+
+        pass
+        
+def zmanjsaj_sliko(slika, sirina, visina):
+    
+    pass
+
+def obdelaj_sliko_s_skatlami(slika, sirina_skatle, visina_skatle, barva_koze) -> list:
+    
+    pass
+
+def prestej_piklse_z_barvo_koze(slika, barva_koze) -> int:
+    
+    pass
+
+
+def doloci_barvo_koze(slika, levo_zgoraj, desno_spodaj) :
+    
+    pass
+
+def pobarvaj_sliko(slika,skatle) :
+
+    pass
+
+
+if __name__ == '__main__':
+    #Pripravi kamero
+
+    #Zajami prvo sliko iz kamere
+
+    #Izračunamo barvo kože na prvi sliki
+
+    #Zajemaj slike iz kamere in jih obdeluj     
+    
+    #Označi območja (škatle), kjer se nahaja obraz (kako je prepuščeno vaši domišljiji)
+        #Vprašanje 1: Kako iz števila pikslov iz vsake škatle določiti celotno območje obraza (Floodfill)?
+        #Vprašanje 2: Kako prešteti število ljudi?
+
+        #Kako velikost prebirne škatle vpliva na hitrost algoritma in točnost detekcije? Poigrajte se s parametroma velikost_skatle
+        #in ne pozabite, da ni nujno da je škatla kvadratna.
+
+
+
+
+    pass
