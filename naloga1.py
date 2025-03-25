1d79913105d1fa7001553af84f9ff93b273f7ff1
Jozef <jozef.kladusek@student.um.si>
2025-03-25 16:59:00 +0100

    Funkcija obdelajsliko s skatlami

--- naloga1.py
+++ naloga1.py
@@ -38,7 +38,38 @@ def zmanjsaj_sliko(slika, sirina, visina):
     pass
 
 def obdelaj_sliko_s_skatlami(slika, sirina_skatle, visina_skatle, barva_koze) -> list:
-    
+    '''Sprehodi se skozi sliko v velikosti škatle (sirina_skatle x visina_skatle) in izračunaj število pikslov kože v vsaki škatli.
+    Škatle se ne smejo prekrivati!
+    Vrne seznam škatel, s številom pikslov kože.
+    Primer: Če je v sliki 25 škatel, kjer je v vsaki vrstici 5 škatel, naj bo seznam oblike
+      [[1,0,0,1,1],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[1,0,0,0,1]]. 
+      V tem primeru je v prvi škatli 1 piksel kože, v drugi 0, v tretji 0, v četrti 1 in v peti 1.'''
+
+    #sprehodim se skozi sliko in sicer naredim 25 skatel sirina 44, visina 68
+    #vsako skatlo prestejem stevilo pikslov, ki ustreza barvi koze in če jih je ve kot 40%
+    #se odločim da je skatla najverjetneje vsebuje barvo koze in dodam 1 v nasprotnem primeru pa dodam 0.
+
+    skatle = []
+    totalpiksel = (sirina_skatle * visina_skatle) * 0.5
+    # Iterate over the image to process each pixel
+    for y in range(0, slika.shape[0], visina_skatle):
+        row = []
+        for x in range(0, slika.shape[1], sirina_skatle):
+
+            slikatemp = slika[y:y + visina_skatle, x:x + sirina_skatle]
+
+            skatla = prestej_piklse_z_barvo_koze(slikatemp, barva_koze)
+
+
+            print(skatla)
+
+            if(skatla>=totalpiksel):
+                row.append(1)
+            else:
+                row.append(0)
+        skatle.append(row)
+
+    return skatle
     pass
 
 def prestej_piklse_z_barvo_koze(slika, barva_koze) -> int:
@@ -158,4 +189,50 @@ if __name__ == '__main__':
             key = cv.waitKey(1) & 0xFF
             if key == ord('q'):
                 break
+                
+                cv.destroyAllWindows()
+        sirina_skatle = int(width/5)
+        visina_skatle = int(height / 5)
+
+
+        barva_koze = (np.array([av_1B, av_1G, av_1R]), np.array([av_2B, av_2G, av_2R]))
+
+        print("sirina,visina skatle: " , sirina_skatle,visina_skatle)
+
+        ''' v barva_koze si zapomnim spodnjo ter zgornjo mejo BGR in v zanki pridobivam sliko kamere in
+          pridobivam skatle in jih na sliki z kvadratom obrišem če je skatla 1  '''
+
+        while True:
+
+
+            ret, slika = kamera.read()
+            slika = zmanjsaj_sliko(slika,260,300)
+            cv.imshow('Kamera', slika)
+            skatle = obdelaj_sliko_s_skatlami(slika, sirina_skatle, visina_skatle, barva_koze)
+            print(skatle)
+            pobarvanaSlika = pobarvaj_sliko(slika, skatle)
+            cv.imshow('Highlighted Image', pobarvanaSlika)
+
+
+
+            if cv.waitKey(1) & 0xFF == ord('q'):
+                break
+
+
+
+            #ret, slika = kamera.read()
+            #slikaTemp = zmanjsaj_sliko(slika, 220, 340)
+            #cv.imshow('Kamera', slikaTemp)
+            #skatle = obdelaj_sliko_s_skatlami(slikaTemp, sirina_skatle, visina_skatle, barva_koze)
+            #print(skatle)
+            #pobarvanaSlika = pobarvaj_sliko(slikaTemp, skatle)
+            #cv.imshow('Highlighted Image', pobarvanaSlika)
+            #cv.waitKey(0)
+            #if key == ord('q'):
+            #    break
+
+        kamera.release()
+        cv.destroyAllWindows()
+
+
     pass
